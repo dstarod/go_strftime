509fe4a75b4e18bef5e13945019dd5ade5f0b779
DMITRY STARODUBTSEV <dmitry.starodubcev@gmail.com>
2015-06-27 23:07:38 +0300

    Perfomance improved: used regexp twice instead of strings.Replace

--- go_strftime.go
+++ go_strftime.go
@@ -3,7 +3,7 @@ package go_strftime
 import(
 	"fmt"
 	"time"
-	"strings"
+	"regexp"
 )
 
 func Strftime(layout string, t time.Time) string {
@@ -11,7 +11,7 @@ func Strftime(layout string, t time.Time) string {
 	// Skipped: %U, %W (not ISO week number) and modifiers E and O
 	// How to use : fmt.Println( Strftime("%Y-%m-%d", time.Now()) )
 	
-	// Timezone and UTC offset
+	// Prepared data: time zone, utc offset, week based year, week number
 	time_zone, utc_offset := t.Zone()
 	minutes_offset := utc_offset/60
 	h := minutes_offset/60
@@ -21,71 +21,151 @@ func Strftime(layout string, t time.Time) string {
 		sign = "-"
 	}
 	iso_offset := fmt.Sprintf("%v%v%02v", sign, h, m)
-	layout = strings.Replace(layout, "%Z", time_zone, 100)
-	layout = strings.Replace(layout, "%z", iso_offset, 100)
-	// mm/dd/yy
-	layout = strings.Replace(layout, "%D", "%m/%d/%y" , 100)	
-	layout = strings.Replace(layout, "%x", "%m/%d/%y" , 100)
-	// Tue Jan 23 12:23:00 2015 	
-	layout = strings.Replace(layout, "%c", "%a %b %e %H:%M:%S %Y" , 100)
-	// YYYY-mm-dd
-	layout = strings.Replace(layout, "%F", "%Y-%m-%d" , 100)
-	// HH:MM:SS AM/PM
-	layout = strings.Replace(layout, "%r", "%I:%M:%S %p" , 100)
-	// HH:MM
-	layout = strings.Replace(layout, "%R", "%H:%M" , 100)
-	// HH:MM:SS
-	layout = strings.Replace(layout, "%T", "%H:%M:%S" , 100)
-	layout = strings.Replace(layout, "%X", "%H:%M:%S" , 100)
-	// Century
-	layout = strings.Replace(layout, "%C", fmt.Sprintf("%v", int(t.Year()/100)), 100)
-	// Day of year
-	layout = strings.Replace(layout, "%j", fmt.Sprintf("%03v", t.YearDay()), 100)
-	// Week based year
+	
 	week_based_year, week_num := t.ISOWeek()
-	layout = strings.Replace(layout, "%G", fmt.Sprintf("%04v", week_based_year), 100)
-	layout = strings.Replace(layout, "%g", fmt.Sprintf("%04v", week_based_year)[2:4], 100)
-	// ISO 8601 week number
-	layout = strings.Replace(layout, "%V", fmt.Sprintf("%v", week_num), 100)	
-	// Year
-	layout = strings.Replace(layout, "%Y", fmt.Sprintf("%04v", t.Year()), 100)
-	layout = strings.Replace(layout, "%y", fmt.Sprintf("%04v", t.Year())[2:4], 100)
-	// Month
-	layout = strings.Replace(layout, "%m", fmt.Sprintf("%02v", int(t.Month())), 100)
-	layout = strings.Replace(layout, "%B", t.Month().String(), 100)
-	switch t.Month(){
-		case time.June, time.July, time.September:
-			layout = strings.Replace(layout, "%b", t.Month().String()[:4], 100)
-			layout = strings.Replace(layout, "%h", t.Month().String()[:4], 100)
-		default:
-			layout = strings.Replace(layout, "%b", t.Month().String()[:3], 100)
-			layout = strings.Replace(layout, "%h", t.Month().String()[:3], 100)
-	}	
-	// Day	
-	layout = strings.Replace(layout, "%d", fmt.Sprintf("%02v", t.Day()), 100)
-	layout = strings.Replace(layout, "%e", fmt.Sprintf("%v", t.Day()), 100)
-	// Weekday
-	layout = strings.Replace(layout, "%A", t.Weekday().String(), 100)
-	layout = strings.Replace(layout, "%a", t.Weekday().String()[:3], 100)
-	layout = strings.Replace(layout, "%u", fmt.Sprintf("%v", int(t.Weekday())), 100)
-	layout = strings.Replace(layout, "%w", fmt.Sprintf("%v", int(t.Weekday())-1 ), 100)
-	// Hour
-	layout = strings.Replace(layout, "%H", fmt.Sprintf("%02v", t.Hour()), 100)
-	layout = strings.Replace(layout, "%I", fmt.Sprintf("%02v", t.Hour() - (t.Hour()/12)*12 ), 100)
-	// Minute
-	layout = strings.Replace(layout, "%M", fmt.Sprintf("%02v", t.Minute()), 100)
-	// Second
-	layout = strings.Replace(layout, "%S", fmt.Sprintf("%02v", t.Second()), 100)
-	// AM/PM
-	if t.Hour()/12 > 0{
-		layout = strings.Replace(layout, "%p", "PM", 100)
-	} else {
-		layout = strings.Replace(layout, "%p", "AM", 100)
-	}
-	// Symbols
-	layout = strings.Replace(layout, "%n", "\n", 100)
-	layout = strings.Replace(layout, "%t", "\t", 100)
-	layout = strings.Replace(layout, "%%", "%", 100)
+
+	// Group patterns	
+	patterns_regexp := regexp.MustCompile("%[DxcFrRTX]{1}")
+	patterns_positions := patterns_regexp.FindAllStringIndex(layout, 1000)
+	
+	var pattern string;
+	var newstr string;
+	pos := 0
+	
+	for _, p := range patterns_positions{
+		newstr += layout[pos:p[0]]
+		pos = p[1]
 		
+		pattern = layout[p[0]:p[1]]
+		switch pattern{
+			case "%D", "%x":
+				newstr += "%m/%d/%y"
+			case "%c":
+				newstr += "%a %b %e %H:%M:%S %Y"
+			case "%F":
+				newstr += "%Y-%m-%d"
+			case "%r":
+				newstr += "%I:%M:%S %p"
+			case "%R":
+				newstr += "%H:%M"
+			case "%T", "%X":
+				newstr += "%H:%M"
+			default:
+				newstr += ""
+		}
+	}
+	
+	if len(newstr) > 0{
+		newstr += layout[pos:]
+		layout = newstr
+	}
+	
+	// Single patterns
+	patterns_regexp = regexp.MustCompile("%[ZzCjGgyYmBbhdeVAauwHIMSpnt%]{1}")
+	patterns_positions = patterns_regexp.FindAllStringIndex(layout, 1000)
+	
+	newstr = ""
+	pos = 0
+	
+	for _, p := range patterns_positions{
+		newstr += layout[pos:p[0]]
+		pos = p[1]
+		
+		pattern = layout[p[0]:p[1]]
+		switch pattern{
+			case "%Z":
+				// Timezone
+				newstr += time_zone
+			case "%z":
+				// UTC offset
+				newstr += iso_offset
+			case "%C":
+				// Century
+				newstr += fmt.Sprintf("%v", int(t.Year()/100))
+			case "%j":
+				// Day of year
+				newstr += fmt.Sprintf("%03v", t.YearDay())
+			case "%G":
+				// Week based year (4 digits)
+				newstr += fmt.Sprintf("%04v", week_based_year)
+			case "%g":
+				// Week based year (last 2 digits)
+				newstr += fmt.Sprintf("%04v", week_based_year)[2:4]
+			case "%y":
+				// Year (last 2 digits)
+				newstr += fmt.Sprintf("%04v", t.Year())[2:4]
+			case "%Y":
+				// Year
+				newstr += fmt.Sprintf("%04v", t.Year())
+			case "%m":
+				// Month (2 digit)
+				newstr += fmt.Sprintf("%02v", int(t.Month()))
+			case "%B":
+				// Month full name
+				newstr += t.Month().String()
+			case "%b", "%h":
+				// Month short name
+				switch t.Month(){
+					case time.June, time.July, time.September:
+						newstr += t.Month().String()[:4]
+					default:
+						newstr += t.Month().String()[:3]
+				}
+			case "%d":
+				// Day of month (2 digit)
+				newstr += fmt.Sprintf("%02v", t.Day())
+			case "%e":
+				// Day of month
+				newstr += fmt.Sprintf("%v", t.Day())
+			case "%V":
+				// ISO 8601 week number
+				newstr += fmt.Sprintf("%v", week_num)
+			case "%A":
+				// Weekday
+				newstr += t.Weekday().String()
+			case "%a":
+				// Weekday short name
+				newstr += t.Weekday().String()[:3]
+			case "%u":
+				// Weekday number (1-7)
+				newstr += fmt.Sprintf("%v", int(t.Weekday()))
+			case "%w":
+				// Weekday numder (0-6)
+				newstr += fmt.Sprintf("%v", int(t.Weekday())-1)
+			case "%H":
+				// Hour (00-24)
+				newstr += fmt.Sprintf("%02v", t.Hour())
+			case "%I":
+				// Hour (00-12)
+				newstr += fmt.Sprintf("%02v", t.Hour() - (t.Hour()/12)*12)
+			case "%M":
+				// Minute (00-59)
+				newstr += fmt.Sprintf("%02v", t.Minute())
+			case "%S":
+				// Second (00-59)
+				newstr += fmt.Sprintf("%02v", t.Second())
+			case "%p":
+				// AM/PM
+				if t.Hour()/12 > 0{
+					newstr += "PM"
+				} else {
+					newstr += "AM"
+				}
+			case "%n":
+				newstr += "\n"
+			case "%t":
+				newstr += "\t"
+			case "%%":
+				newstr += "%"
+			default:
+				newstr += ""
+		}
+	}
+	
+	if len(newstr) > 0{
+		newstr += layout[pos:]
+		layout = newstr
+	}
+
 	return layout
 }
